1a584c1eb6ec8247061535c65504648c4279e670
skipadoodle <253850851+eak9973-fish@users.noreply.github.com>
2026-04-25 00:49:06 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -10,12 +10,34 @@ public class Akaei extends Application {
 
     @Override
     public void start(Stage stage) {
-        // Create WebView and engine
+
+        TabPane tabPane = new TabPane();
+
+
+        tabPane.getTabs().add(createNewTab("https://www.google.com"));
+
+        Button newTabButton = new Button("+");
+        newTabButton.setOnAction(e -> {
+            Tab newTab = createNewTab("https://www.google.com");
+            tabPane.getTabs().add(newTab);
+            tabPane.getSelectionModel().select(newTab);
+        });
+
+        BorderPane root = new BorderPane();
+        root.setTop(newTabButton);
+        root.setCenter(tabPane);
+
+        Scene scene = new Scene(root, 1000, 700);
+        stage.setTitle("Akaei");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private Tab createNewTab(String defaultUrl) {
         WebView webView = new WebView();
         WebEngine webEngine = webView.getEngine();
 
-        // URL bar
-        TextField urlField = new TextField("https://www.priv.au);
+        TextField urlField = new TextField(defaultUrl);
 
         urlField.setOnAction(e -> {
             String url = urlField.getText();
@@ -25,19 +47,22 @@ public class Akaei extends Application {
             webEngine.load(url);
         });
 
-        // Layout
-        BorderPane root = new BorderPane();
-        root.setTop(urlField);
-        root.setCenter(webView);
+        BorderPane layout = new BorderPane();
+        layout.setTop(urlField);
+        layout.setCenter(webView);
 
-        // Load default page
-        webEngine.load(urlField.getText());
+        webEngine.load(defaultUrl);
 
-        
-        Scene scene = new Scene(root, 1000, 700);
-        stage.setTitle("Test complete");
-        stage.setScene(scene);
-        stage.show();
+        Tab tab = new Tab("New Tab");
+        tab.setContent(layout);
+        tab.setClosable(true);
+
+      
+        webEngine.titleProperty().addListener((obs, oldTitle, newTitle) -> {
+            tab.setText(newTitle);
+        });
+
+        return tab;
     }
 
     public static void main(String[] args) {
